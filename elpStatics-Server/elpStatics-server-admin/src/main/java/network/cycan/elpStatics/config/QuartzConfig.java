bb4ed25693bf1761cc5765d08a30b353b498bbe5
blustin <winlinking@163.com>
2021-04-30 12:17:07 +0800

    feat:添加定时任务

---
+++ elpStatics-Server/elpStatics-server-admin/src/main/java/network/cycan/elpStatics/config/QuartzConfig.java
@@ -0,0 +1,29 @@
+package network.cycan.elpStatics.config;
+
+import network.cycan.elpStatics.job.StartOfDayJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class QuartzConfig {
+    @Bean
+    public JobDetail jobDetail() {
+        JobDetail jobDetail = JobBuilder.newJob(StartOfDayJob.class)
+                .withIdentity("start_of_day", "start_of_day")
+                .storeDurably()
+                .build();
+        return jobDetail;
+    }
+
+    @Bean
+    public Trigger trigger() {
+        Trigger trigger =TriggerBuilder.newTrigger().forJob(jobDetail()).withIdentity("start_of_day", "start_of_day")
+                .startNow()
+                // 每天0点执行
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?"))
+                .build();
+        return trigger;
+    }
+}
---
+++ elpStatics-Server/elpStatics-server-admin/src/main/java/network/cycan/elpStatics/job/StartOfDayJob.java
@@ -0,0 +1,31 @@
+package network.cycan.elpStatics.job;
+
+import lombok.extern.slf4j.Slf4j;
+import network.cycan.core.util.DateUtils;
+import network.cycan.elpStatics.enums.ChainContractType;
+import network.cycan.elpStatics.service.IBlockChainService;
+import network.cycan.elpStatics.util.BlockChainUtil;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+
+@Slf4j
+@Component
+public class StartOfDayJob extends QuartzJobBean {
+    @Autowired
+    private IBlockChainService iBlockChainService;
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext)
+            throws JobExecutionException {
+       log.info("定时任务启动====="+DateUtils.getNowTime(DateUtils.YYYY_MM_DD_HH_MM_SS));
+        iBlockChainService.saveTodayBlockData(DateUtils.today(), BlockChainUtil.ELP_CONTRACT_ADDREES, ChainContractType.ELP.getType());
+        iBlockChainService.saveTodayBlockData(DateUtils.today(), BlockChainUtil.LP_TOKEN_ADDRESS, ChainContractType.LP.getType());
+        log.info("定时任务结束====="+DateUtils.getNowTime(DateUtils.YYYY_MM_DD_HH_MM_SS));
+
+    }
+}
